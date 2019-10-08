9a9910f3826a7576f0be1440dd4be0e4c42ebea3
Bruce Nishimura <bruceninga@gmail.com>
2019-10-07 21:05:00 -0400

    Added create_account endpoint and TODOs

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import constants
+import re
 
 user_status = constants.idle_state
 def main():
@@ -15,8 +16,11 @@ def main():
         #logout command
         elif(user_command == constants.logout_command):
             logout()
-        #
+        #createacct
+        elif(user_command == constants.createacct_command):
+            create_account()
 
+#TODO: open file when login
 def login():
     global user_status
     if(user_status != constants.idle_state):
@@ -25,9 +29,13 @@ def login():
     while True:
         user_input = input(constants.login_message)
         parsed_login = user_input.lower().strip()
-        if(parsed_login == constants.atm_state or parsed_login == constants.agent_state):
-            user_status = parsed_login
-            print(constants.successful_login(parsed_login))
+        if(parsed_login == constants.atm_state):
+            user_status = constants.atm_state
+            print(constants.successful_login(constants.atm_state))
+            return
+        elif(parsed_login == constants.agent_state):
+            user_status = constants.agent_state
+            print(constants.successful_login(constants.agent_state))
             return
         elif(parsed_login == cancel_command):
             return
@@ -38,8 +46,41 @@ def logout():
     global user_status
     if(user_status == constants.idle_state):
         print(constants.logged_out_logout_message)
+        return
     else:
         print(constants.logout_message)
         user_status = constants.idle_state
+    #TODO: Write to file on logout
+def create_account():
+    global user_status
+    if(user_status != constants.agent_state):
+        print(constants.create_account_without_privilege)
+        return
+    account_number = None
+    account_name = None
+    while True:
+        account_number = input(constants.account_number_input)
+        if(valid_account_number(account_number)):
+            break
+        elif account_number == constants.cancel_command:
+            return
+        else:
+            print(constants.invalid_account_number)
+    while True:
+        account_name = input(constants.account_name_input)
+        if(valid_account_name(account_name)):
+            break
+        elif account_name == constants.cancel_command:
+            return
+        else:
+            print(constants.invalid_account_name)
+    #TODO: Actually write this command to file.
+    
+def valid_account_name(name):
+    return name is not None and re.search('^[\w][\w ]{1,28}[\w]$', name) is not None
+def valid_account_number(number):
+    return number is not None and re.search('^[1-9][0-9]{6}$', number) is not None
+
+
 if __name__ == "__main__":
     main()
--- constants.py
+++ constants.py
@@ -17,7 +17,12 @@ withdraw_command = 'withdraw'
 transfer_command = 'transfer'
 privileged_commands = [createacct_command, deleteacct_command]
 atm_commands = [login_command, logout_command, deposit_command, withdraw_command, transfer_command]
-login_message = 'Select session type ('+atm_state+' or '+agent_state + '):'
+login_message = 'Select session type ('+atm_state+' or '+agent_state + '): '
+create_account_without_privilege = 'Error: ' + agent_state + ' session required for createacct command'
+invalid_account_name = 'Error: invalid account name. Account number must be between 3 and 30 alphanumeric characters, not beginning or ending with a space'
+account_number_input = 'Account Number: '
+account_name_input = 'Account Name: '
+invalid_account_number = 'Invalid account number, must be 7 digits, not beginning with a 0'
 def successful_login(session_type):
     return 'Successfully logged in as "' + session_type +'"'
 def unrecognized_login_command(command):
