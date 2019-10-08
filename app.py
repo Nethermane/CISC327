eaee412b327cb52857284dcb0935d450a63416bd
Bruce Nishimura <bruceninga@gmail.com>
2019-10-08 01:32:39 -0400

    Added all front-end except file related matters

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import constants
 import re
 
 user_status = constants.idle_state
+current_file_output = []
+accounts_list = ['1234567']
 def main():
     print(constants.first_launch_message)
     while True:
@@ -10,17 +12,33 @@ def main():
         #Prevent all commands but login before logging in
         if(user_status == constants.idle_state and parsed_command != constants.login_command):
             print(constants.not_logged_in_message)
-        #Login command
+        #login
         elif(user_command == constants.login_command):
             login()
-        #logout command
+        #logout
         elif(user_command == constants.logout_command):
             logout()
         #createacct
         elif(user_command == constants.createacct_command):
             create_account()
+        #deleteacct
+        elif(user_command == constants.deleteacct_command):
+            delete_account()
+        #deposit
+        elif(user_command == constants.deposit_command):
+            deposit()
+        #withdraw
+        elif(user_command == constants.withdraw_command):
+            withdraw()
+        #transfer
+        elif(user_command == constants.transfer_command):
+            transfer()
+        elif(user_command == constants.help_command):
+            print(constants.help_text)
+        else:
+            print(constants.unrecognized_command)
 
-#TODO: open file when login
+#TODO: open valid accounts file when login
 def login():
     global user_status
     if(user_status != constants.idle_state):
@@ -37,7 +55,7 @@ def login():
             user_status = constants.agent_state
             print(constants.successful_login(constants.agent_state))
             return
-        elif(parsed_login == cancel_command):
+        elif(parsed_login == constants.cancel_command):
             return
         else:
             user_status = constants.idle_state
@@ -49,37 +67,176 @@ def logout():
         return
     else:
         print(constants.logout_message)
+        current_file_output.append([constants.end_of_file_key,
+                            constants.empty_account_number,
+                            constants.empty_money_amount,
+                            constants.empty_account_number,
+                            constants.empty_account_name
+                            ])
+        print(current_file_output)
+        current_file_output = []
+        #TODO: Write to file, currently just prints
         user_status = constants.idle_state
-    #TODO: Write to file on logout
 def create_account():
     global user_status
     if(user_status != constants.agent_state):
         print(constants.create_account_without_privilege)
         return
-    account_number = None
-    account_name = None
-    while True:
-        account_number = input(constants.account_number_input)
-        if(valid_account_number(account_number)):
-            break
-        elif account_number == constants.cancel_command:
-            return
-        else:
-            print(constants.invalid_account_number)
-    while True:
-        account_name = input(constants.account_name_input)
-        if(valid_account_name(account_name)):
-            break
-        elif account_name == constants.cancel_command:
-            return
-        else:
-            print(constants.invalid_account_name)
-    #TODO: Actually write this command to file.
-    
+    account_number = get_valid_account_number()
+    if(account_number is None):
+        return
+    account_name = get_valid_account_name()
+    if(account_name is None):
+        return
+    current_file_output.append([constants.create_account_output_key,
+                                account_number,
+                                empty_money_amount,
+                                constants.empty_account_number,
+                                account_name
+                                ])
+    print(constants.successful_delete)
+
+def delete_account():
+    global user_status
+    if(user_status != constants.agent_state):
+        print(constants.create_account_without_privilege)
+        return
+    account_number = get_valid_account_number()
+    if(account_number is None):
+        return
+    account_name = get_valid_account_name()
+    if(account_name is None):
+        return
+    current_file_output.append([constants.delete_account_output_key,
+                                account_number,
+                                empty_money_amount,
+                                constants.empty_account_number,
+                                account_name
+                                ])
+    print(constants.successful_delete)
+
+def deposit():
+    global user_status, current_file_output
+    if(user_status == constants.idle_state):
+        print(constants.must_be_signed_in_for_command(constants.deposit_command))
+        return
+    account_number = get_valid_account_number(True)
+    #Check number less than max single transaction
+    cents = get_valid_numeric_amount(constants.max_deposit_atm_once if user_status == constants.atm_state else constants.max_deposit_teller_once)
+    if(cents is None):
+        return
+    #Check if total deposits that day less than total
+    if(user_status == constants.agent_state or total_less_than_daily_limit(constants.deposit_output_key, account_number, constants.max_deposit_atm_daily)):
+        current_file_output.append([constants.deposit_output_key,
+                                    account_number,
+                                    cents,
+                                    constants.empty_account_number,
+                                    constants.empty_account_name
+                                    ])
+        print(constants.successful_deposit)
+    else:
+        print(constants.deposit_over_max)
+
+def withdraw():
+    global user_status, current_file_output
+    if(user_status == constants.idle_state):
+        print(constants.must_be_signed_in_for_command(constants.withdraw_command))
+        return
+    account_number = get_valid_account_number(True)
+    #Check number less than max single transaction
+    cents = get_valid_numeric_amount(constants.max_withdraw_atm_once if user_status == constants.atm_state else constants.max_withdraw_agent_once)
+    if(cents is None):
+        return
+    #Check if total withdrawl less than total limit
+    if(user_status == constants.agent_state or total_less_than_daily_limit(constants.withdraw_output_key, account_number, constants.max_withdraw_atm_daily)):
+        current_file_output.append([constants.deposit_output_key,
+                                    constants.empty_account_number,
+                                    cents,
+                                    account_number,
+                                    constants.empty_account_name
+                                    ])
+        print(constants.successful_withdraw)
+    else:
+        print(constants.withdraw_over_max)
+
+def transfer():
+    global user_status, current_file_output
+    if(user_status == constants.idle_state):
+        print(constants.must_be_signed_in_for_command(constants.withdraw_command))
+        return
+    from_account = get_valid_account_number(True, constants.from_account_prompt)
+    to_number = get_valid_account_number(True, constants.to_account_prompt)
+    #Check number less than max single transaction
+    cents = get_valid_numeric_amount(constants.max_transfer_atm_once if user_status == constants.atm_state else constants.max_transfer_agent_once)
+    if(cents is None):
+        return
+    #Check if total withdrawl less than total limit
+    if(user_status == constants.agent_state or total_less_than_daily_limit(constants.transfer_output_key, account_number, constants.max_transfer_atm_once)):
+        current_file_output.append([constants.deposit_output_key,
+                                    to_account,
+                                    cents,
+                                    from_account,
+                                    constants.empty_account_name
+                                    ])
+        print(constants.successful_withdraw)
+    else:
+        print(constants.withdraw_over_max)
+
+
 def valid_account_name(name):
     return name is not None and re.search('^[\w][\w ]{1,28}[\w]$', name) is not None
 def valid_account_number(number):
     return number is not None and re.search('^[1-9][0-9]{6}$', number) is not None
+def valid_cents_amount(number, max_value):
+    return number is not None and number <= max_value
+def total_less_than_daily_limit(transaction_type, account_number, limit):
+    return sum(map(lambda row: int(row[2]),filter(lambda row: row[0] == transaction_type, current_file_output))) <= limit
+    
+#returns a valid account number from user input
+#If check_accounts_list is True, requires the number to be in the accounts list
+#returns None if exit command is provided
+def get_valid_account_number(check_accounts_list, prompt=constants.account_number_input):
+    while True:
+        account_number = input(prompt)
+        if(valid_account_number(account_number)):
+            if((not check_accounts_list) or (check_accounts_list and account_number in accounts_list)):
+                return account_number
+            else:
+                print(constants.account_number_not_in_list)
+        elif account_number == constants.cancel_command:
+            return None
+        else:
+            print(constants.invalid_account_number)
+
+#returns a valid account name from user input
+#returns None if exit command is provided
+def get_valid_account_name():
+    while True:
+        account_name = input(constants.account_name_input)
+        if(valid_account_name(account_name)):
+            return account_name
+        elif account_name == constants.cancel_command:
+            return None
+        else:
+            print(constants.invalid_account_name)
+
+#returns a valid cents amount less than or equal to max value
+#returns None if exit command provided
+#returns a string, ensuring it is validly numeric.
+def get_valid_numeric_amount(max_value):
+    while True:
+        cents = input(constants.cents_value).replace(',','')
+        try:
+            cents_int = int(cents)
+            if(valid_cents_amount(cents_int, max_value)):
+                return str(cents_int)
+            else:
+                print(constants.error_cents_less_than(str(max_value)))
+        except ValueError:
+            if cents == constants.cancel_command:
+                return None
+            print(constants.parse_number_error)
+
 
 
 if __name__ == "__main__":
--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 first_launch_message = 'Welcome to Quinterac banking, type login to begin'
+unrecognized_command = 'Error: unrecognized command'
 not_logged_in_message = 'In idle state, please login before use'
 logged_out_logout_message = 'Error not logged in'
 logged_in_login = 'Error: already logged in'
@@ -15,15 +16,50 @@ deleteacct_command = 'deleteacct'
 deposit_command = 'deposit'
 withdraw_command = 'withdraw'
 transfer_command = 'transfer'
+help_command = 'help'
 privileged_commands = [createacct_command, deleteacct_command]
 atm_commands = [login_command, logout_command, deposit_command, withdraw_command, transfer_command]
+help_text = 'Accepted commands: ' + ', '.join(atm_commands) + ', ' + ', '.join(privileged_commands)
 login_message = 'Select session type ('+atm_state+' or '+agent_state + '): '
 create_account_without_privilege = 'Error: ' + agent_state + ' session required for createacct command'
 invalid_account_name = 'Error: invalid account name. Account number must be between 3 and 30 alphanumeric characters, not beginning or ending with a space'
 account_number_input = 'Account Number: '
 account_name_input = 'Account Name: '
 invalid_account_number = 'Invalid account number, must be 7 digits, not beginning with a 0'
+account_number_not_in_list = 'Error: accout number not found'
+cents_value = 'Amount(cents): '
+deposit_output_key = 'DEP'
+withdraw_output_key = 'WDR'
+transfer_output_key = 'XFR'
+new_account_output_key = 'NEW'
+delete_account_output_key = 'DEL'
+end_of_file_key = 'EOS'
+empty_account_number = '0000000'
+empty_money_amount = '000'
+empty_account_name = '***'
+max_deposit_atm_once = 200000
+max_deposit_atm_daily = 500000
+max_deposit_teller_once = 99999999
+max_withdraw_atm_once = 100000
+max_withdraw_atm_daily = 500000
+max_withdraw_agent_once = 99999999
+max_transfer_atm_once = 1000000
+max_transfer_atm_daily = 1000000
+max_transfer_agent_once = 99999999
+successful_deposit = 'Deposit successful'
+successful_withdraw = 'Withdraw successful'
+successful_delete = 'Delete sucessful'
+deposit_over_max = 'Error: deposit over limit'
+withdraw_over_max = 'Error: withdraw over limit'
+from_account_prompt = 'From: '
+to_account_prompt = 'To: '
+
+parse_number_error = 'Error parsing input'
 def successful_login(session_type):
     return 'Successfully logged in as "' + session_type +'"'
 def unrecognized_login_command(command):
     return 'Error unrecognized command: "'+command+'", login with either ' + atm_state + ' or ' + agent_state
+def must_be_signed_in_for_command(command):
+    return 'Command: "' + command + '" requires logged in session'
+def error_cents_less_than(max_value):
+    return 'Error: must be less than or equal to ' + max_value + ' cents'
